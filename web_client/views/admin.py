1732a6ee2084187abec93f907579b9809bbd3179
Scion-Kin <dinturner17@gmail.com>
2024-04-22 16:41:13 +0200

    Admin panel protected

--- web_client/views/admin.py
+++ web_client/views/admin.py
@@ -2,7 +2,7 @@
 ''' The admin manager '''
 
 from web_client.views import client_view
-from flask import render_template, request, session, url_for, redirect
+from flask import render_template, request, session, url_for, redirect, abort
 from models import storage
 from itsdangerous import URLSafeSerializer
 import requests
@@ -11,33 +11,38 @@ import requests
 @client_view.route('/manage/authors', strict_slashes=False)
 def manage_authors():
     ''' manage authors '''
-
-    return render_template('manage_resource.html', title="Authors")
+    if session and session['user_type'] == 'king':
+        return render_template('manage_resource.html', title="Authors")
+    abort(404)
 
 
 @client_view.route('/manage/books', strict_slashes=False)
 def manage_books():
     ''' manage books '''
-
-    return render_template('manage_resource.html', title="Books")
+    if session and session['user_type'] == 'king':
+        return render_template('manage_resource.html', title="Books")
+    abort(404)
 
 
 @client_view.route('/manage/genres', strict_slashes=False)
 def manage_genres():
     ''' manage genres '''
-
-    return render_template('manage_resource.html', title="Genres")
+    if session and session['user_type'] == 'king':
+        return render_template('manage_resource.html', title="Genres")
+    abort(404)
 
 
 @client_view.route('/manage/reviews', strict_slashes=False)
 def manage_reviews():
     ''' manage reviews '''
-
-    return render_template('manage_resource.html', title="Reviews")
+    if session and session['user_type'] == 'king':
+        return render_template('manage_resource.html', title="Reviews")
+    abort(404)
 
 
 @client_view.route('/manage/users', strict_slashes=False)
 def manage_users():
     ''' manage users '''
-
-    return render_template('manage_resource.html', title="Users")
+    if session and session['user_type'] == 'king':
+        return render_template('manage_resource.html', title="Users")
+    abort(404)
