3c2aa11d00da2b278a68dbbeb0bb4c012adbf005
Scion-Kin <dinturner17@gmail.com>
2024-04-23 15:53:20 +0200

    Some minor nesting correction.

--- web_client/static/scripts/user_manager.js
+++ web_client/static/scripts/user_manager.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    const logOut = document.getElementById('log-out');
+
+    logOut.addEventListener('click', function () {
+        window.location.href = '/logout';
+    });
+
 });
