dbbfde0830d9243f1ecf18ba1b5268b9c35cbfb2
Scion-Kin <dinturner17@gmail.com>
2024-04-24 13:08:28 +0200

    Book description page done.

--- web_client/static/scripts/info.js
+++ web_client/static/scripts/info.js
@@ -14,22 +14,48 @@ $(function () {
                     let resource = document.createElement('section');
                     const resource_title = document.createElement('h4');
                     const resource_data_container = document.createElement('section');
+                    const form = document.createElement('form');
+                    const section = document.createElement('section');
                     const toInfo = document.createElement('button');
                     const toRead = document.createElement('button');
                     const img = document.createElement('img');
                     let resource_data = data[i].data;
 
-                    $(img).attr('src', `/static/images/${resource_data.pic}`);
+                    $(toRead).click(function () {
+                        window.location.href = `/read/${resource_data.id}`;
+                    });
 
+                    $(form).attr('method', 'POST');
+                    $(img).attr('src', `/static/images/${resource_data.pic}`);
+                    $(toInfo).text('Info & reviews');
+                    $(toInfo).attr({'name': 'id', 'value': resource_data.id});
+                    $(toRead).text('Read');
+                    $(form).append(toInfo);
+                    $(section).append(toRead);
                     $(resource_data_container).addClass('data');
                     $(resource_title).text(i);
                     $(resource_data_container).append([img,  resource_title]);
-                    $(resource).append(resource_data_container);
+                    $(resource).append([resource_data_container, form, section]);
                     $(resource).addClass('resource');
                     $(resource).attr('id', resource_data.id);
                     $("#search_results").append(resource);
                 }
             }
         });
+    }
+
+    else {
+        if ($('#reviews')) {
+            let reviews = $('#reviews').children();
+            for (let i = 0; i < reviews.length; i++) {
+                let imgId = $(reviews[i]).find('img').attr('id');
+                $.get(`http://localhost:5000/api/v1/users/${imgId}`, function(data, textStatus) {
+                    for (let j in data) {
+                        $(`#${imgId}`).attr('src', '/static/images/' + data[j].data.pic);
+                        break;
+                    }
+                });
+            }
+        }
     }
 });
--- web_client/views/info.py
+++ web_client/views/info.py
@@ -4,18 +4,27 @@
 from web_client.views import client_view
 from flask import Flask, Blueprint, render_template, abort, session, request, abort, redirect, url_for
 import requests
+from uuid import uuid4
 
 
 @client_view.route('/about/book', methods=['GET', 'POST'], strict_slashes=False)
 def book_info():
     ''' The home page route '''
 
-    if request.method == 'POST':
+    if session and session['logged'] == True:
+        if request.method == 'POST':
 
-        response = requests.get('http://localhost:5000/api/v1/books/{}'.format(request.form.get('id')))
+            response = requests.get('http://localhost:5000/api/v1/books/{}'.format(request.form.get('id')))
 
-        if response.status_code < 300:
+            if response.status_code < 300:
+                for i in response.json():
 
-            return render_template('info.html', info=response.json())
+                    reviews = requests.get('http://localhost:5000/api/v1/{}/reviews'.format(request.form.get('id')))
 
-    return render_template('info.html')
+                    if reviews.status_code == 200:
+                        return render_template('info.html', info=response.json()[i]["data"], reviews=reviews.json())
+                    return render_template('info.html', info=response.json()[i]["data"]) 
+
+        return render_template('info.html')
+
+    return redirect(url_for('home'))
