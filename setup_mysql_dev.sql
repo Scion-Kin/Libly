bbb321de0719e78699021995a6bb9caf79f55df0
Scion-Kin <dinturner17@gmail.com>
2024-04-25 23:02:26 +0200

    Some mistake in sql script corrected

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE IF NOT EXISTS libly;
 CREATE USER IF NOT EXISTS 'libly_user'@'localhost' IDENTIFIED BY 'libDev';
 GRANT ALL PRIVILEGES ON `libly`.* TO 'libly_user'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'libly'@'localhost';
 FLUSH PRIVILEGES;
