5fe950d32427d72724b040362ec348267f1592ab
Scion-Kin <dinturner17@gmail.com>
2024-04-26 10:23:17 +0200

    Some css change

--- web_client/static/scripts/user_manager.js
+++ web_client/static/scripts/user_manager.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const logOut = document.getElementById('log-out');
+    const showMenu = document.getElementById('user-menu');
+    const user = document.getElementById('user');
+
+    let displayed = false;
+
+    user.addEventListener('click', function () {
+        if (displayed == false) {
+            showMenu.style.display = 'block';
+            displayed = true;
+        }
+        else {
+            showMenu.style.display = 'none';
+            displayed = false;
+        }
+    });
 
     logOut.addEventListener('click', function () {
         window.location.href = '/logout';
