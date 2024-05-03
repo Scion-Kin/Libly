3b9653df926b6ffdad3cd40b22ecd2e0c37eef72
Scion-Kin <dinturner17@gmail.com>
2024-05-03 13:21:37 +0200

    Major update on favoriting.

--- web_client/static/scripts/user_manager.js
+++ web_client/static/scripts/user_manager.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const user = document.getElementById('avatar');
     const settings = document.getElementById('settings');
     const help = document.getElementById('help');
-    const bread = document.getElementById('breadcrumb');
+    const love = document.getElementById('love');
 
     settings.addEventListener('click', function () {
         window.location.href = '/settings';
@@ -31,6 +31,88 @@ document.addEventListener('DOMContentLoaded', function () {
     logOut.addEventListener('click', function () {
         window.location.href = '/logout';
     });
+
+    if (love) {
+        let user_id = getUserId();
+        let typeId = love.getAttribute('type-id');
+        let favType = love.getAttribute('fav');
+        let favId = love.getAttribute('fav-id');
+        let stripped = favType.replace(/s/g, '_id'); // remove the 's' at the end to interact with the response better
+        console.log(stripped);
+
+        fetch(`https://usernet.tech/api/v1/${user_id}/favs/${favType}`)
+            .then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(function (data) { 
+                for (i in data) {
+                    if (i[stripped] == typeId) {
+                        love.style.backgroundImage = "url('/static/images/loved-icon.svg')";
+                        love.setAttribute('fav-id', i["id"]);
+                        break;
+                    }
+                }
+            })
+            .catch(error => {
+                alert('Failed to fetch data or parse response');
+            });
+
+        love.addEventListener('click', function () {
+            if (love.style.backgroundImage == '/static/images/loved-icon.svg') {
+                fetch(`https://usernet.tech/api/v1/favs/${favType}/${favId}`, {
+                    method: 'DELETE'
+                })
+                .then(function(response) {
+                    if (response.ok) {
+                        love.style.backgroundImage = "url('/static/images/love-icon.svg')";
+                        love.removeAttribute('fav-id');
+                    } else {
+                        alert('Failed to remove favorite');
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            } else {
+                let content = {}
+                if (stripped === 'author_id') {
+                    content = {
+                        author_id: typeId,
+                        user_id: user_id
+                    }
+                } else if (stripped === 'genre_id') {
+                    content = {
+                        genre_id: typeId,
+                        user_id: user_id
+                    }
+                } else {
+                    content = {
+                        book_id: typeId,
+                        user_id: user_id
+                    }
+                }
+
+                fetch(`https://usernet.tech/api/v1/favs/${favType}/`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(content)
+                })
+                .then(function(response) {
+                    if (response.ok) {
+                        love.style.backgroundImage = "url('/static/images/loved-icon.svg')";
+                        love.setAttribute('fav-id', response.json()['id']);
+                    } else {
+                        alert('Failed to make favorite');
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            }
+        });
+    }
 
     let history = document.getElementById('history');
 
@@ -63,6 +145,17 @@ document.addEventListener('DOMContentLoaded', function () {
                     section.style.backgroundImage = `url('/static/images/${localStorage.getItem(key).split('_')[1]}')`;
                     history.appendChild(section);
                 }
+            }
+        }
+    }
+
+    function getUserId() {
+        let cookieArray = document.cookie.split(';');
+
+        for (let i = 0; i < cookieArray.length; i++) {
+            let cookie = cookieArray[i].trim();
+            if (cookie.indexOf('user_id') == 0) {
+                return cookie.substring(('=user_id').length, cookie.length);
             }
         }
     }
