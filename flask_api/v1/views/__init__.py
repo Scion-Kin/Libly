6e7745cdbee83ea520502b6fc972f298cd048102
Scion-Kin <dinturner17@gmail.com>
2024-05-05 19:12:54 +0200

    New feature added. User hot list based on reviews count.

--- flask_api/v1/views/__init__.py
+++ flask_api/v1/views/__init__.py
@@ -4,14 +4,15 @@ from flask import Blueprint
 
 grand_view = Blueprint('grand_view', __name__, url_prefix="/api/v1")
 
-from flask_api.v1.views.book import *
-from flask_api.v1.views.index import *
-from flask_api.v1.views.genre import *
 from flask_api.v1.views.author import *
-from flask_api.v1.views.review import *
-from flask_api.v1.views.user import *
-from flask_api.v1.views.search import *
+from flask_api.v1.views.book import *
 from flask_api.v1.views.fav_author import *
 from flask_api.v1.views.fav_book import *
 from flask_api.v1.views.fav_genre import *
+from flask_api.v1.views.genre import *
+from flask_api.v1.views.hot import *
+from flask_api.v1.views.index import *
 from flask_api.v1.views.report import *
+from flask_api.v1.views.review import *
+from flask_api.v1.views.search import *
+from flask_api.v1.views.user import *
