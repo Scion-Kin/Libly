0f2eb9e2cd4260e59d1179c063d4d5bbae14f0db
Scion-Kin <dinturner17@gmail.com>
2024-06-07 16:48:34 +0200

    Removed error in server setup script

--- deployment/package_install.sh
+++ deployment/package_install.sh
@@ -5,5 +5,6 @@ sudo apt-get install python3
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install certbot
+sudo apt install pkg-config
 pip3 install -r requirements.txt
-npm install -g nodemailer express body-parser cors
+sudo npm install -g nodemailer express body-parser cors
