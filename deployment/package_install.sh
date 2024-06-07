cd0badfa157f2f9d93a7016c6d7604105bfc84f8
Scion-Kin <dinturner17@gmail.com>
2024-06-07 16:57:08 +0200

    Removed error in package installs script

--- deployment/package_install.sh
+++ deployment/package_install.sh
@@ -5,6 +5,7 @@ sudo apt-get install python3
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install certbot
-sudo apt install pkg-config
+sudo apt-get install mysql-client
+sudo apt install pkg-config python3-dev default-libmysqlclient-dev build-essential
 pip3 install -r requirements.txt
 sudo npm install -g nodemailer express body-parser cors
