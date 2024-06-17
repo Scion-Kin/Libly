943bf03f2860f68229566f7c046a5bf460f10a0c
Scion-Kin <dinturner17@gmail.com>
2024-06-17 11:30:32 +0200

    Install script now using only apt-get instead of apt

--- deployment/package_install.sh
+++ deployment/package_install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt install nginx -y
+sudo apt-get install nginx -y
 sudo apt-get install gunicorn -y
 sudo apt-get install python3 -y
-sudo apt install python3-pip
+sudo apt-get install python3-pip -y
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install certbot python3-certbot-nginx -y
 sudo apt-get install mysql-client mysql-server -y
-sudo apt install pkg-config python3-dev default-libmysqlclient-dev build-essential -y
+sudo apt-get install pkg-config python3-dev default-libmysqlclient-dev build-essential -y
 pip3 install -r requirements.txt
 sudo npm install -g nodemailer express body-parser cors
