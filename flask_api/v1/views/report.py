7527d8f32d8cf132032ba9a56c67b284d5138cc2
Scion-Kin <dinturner17@gmail.com>
2024-08-02 11:34:55 +0200

    Solved error in report.py

--- flask_api/v1/views/report.py
+++ flask_api/v1/views/report.py
@@ -4,7 +4,6 @@ from flask_api.v1.views import grand_view
 from models import storage
 from flask import jsonify, make_response, request
 from datetime import datetime, timedelta
-import calendar
 
 
 @grand_view.route('/report/days/<int:days>', strict_slashes=False)
@@ -15,7 +14,7 @@ def get_report(days):
     full = [i.to_dict() for i in storage.all().values() if
             i.created_at >= period]
 
-    obs = {"authors", "books", "genres", "reviews", "users"}
+    obs = {"Author", "Book", "Genre", "Review", "User"}
     report = {i: [j for j in full if j["__class__"] == i] for i in obs}
 
     return jsonify(dict(sorted(report.items(), key=lambda item: item[0])))
@@ -27,16 +26,13 @@ def year_report(year):
     if year < 2024 or year > datetime.now().year:
         return make_response(jsonify({"error": "Invalid year given"}), 400)
 
-    days = 366 if calendar.isleap(year) else 365
-    period = datetime.now() - timedelta(days=days)
-    full = [i for i in storage.all().values() if i.created_at >= period]
-
+    full = [i for i in storage.all().values() if i.created_at.year == year]
     all_months = []
 
     # get data from all months
     for i in range(1, 13):
         month = [j.to_dict() for j in full if j.created_at.month == i]
-        obs = {"authors", "books", "genres", "reviews", "users"}
+        obs = {"Author", "Book", "Genre", "Review", "User"}
         report = {k: [j for j in month if j["__class__"] == k] for k in obs}
 
         all_months.append(dict(sorted(report.items(),
